bc4b921a9664143362276fc95e251396ef0bb7fb
Fayzan Ali Akhtar <fayzan585@gmail.com>
2025-05-07 01:32:48 +0500

    starting report

---
+++ Report_Blog/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gradient-to-r from-blue-500 to-purple-600">
+      <h1 className="text-4xl font-bold text-white">
+      ✨ Sproj 🤖!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
